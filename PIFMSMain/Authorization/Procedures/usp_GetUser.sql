b1cb9594bb08025dd31ab221906072e9c4b6afd2
H294067 <vinay.kr@honeywell.com>
2019-06-21 16:57:55 +0530

    Added User and Role stored procedures

---
+++ PIFMSMain/Authorization/Procedures/usp_GetUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [Authorization].[usp_GetUser]
+	@name NVARCHAR(100)
+AS
+	SELECT 
+		Id, 
+		FirstName, 
+		LastName, 
+		Email, 
+		PhoneNumber, 
+		LastLoggedin  
+	FROM [Authorization].[Users] 
+	WHERE 
+		Email like '%'+@name+'%'
+		OR FirstName like '%'+@name+'%'
+		OR LastName like '%'+@name+'%'
+RETURN 0
